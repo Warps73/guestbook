0b69828acf3c5cc6facdf6161e7d4205853a3485
Prefol Timothy <timothy.prefol@gmail.com>
2020-04-06 20:21:20 +0200

    easy_admin

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -16,6 +16,14 @@ use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
+
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
      * @param Request $request
@@ -28,11 +36,10 @@ class ConferenceController extends AbstractController
      */
     public function index(
         Request $request,
-        Environment $twig,
         ConferenceRepository $conferenceRepository
     ) {
         return new Response(
-            $twig->render(
+            $this->twig->render(
                 'conference/index.html.twig',
                 [
                     'conferences' => $conferenceRepository->findAll(),
@@ -44,6 +51,7 @@ class ConferenceController extends AbstractController
 
     /**
      * @Route("/conference/{id}", name="conference")
+     * @param Request $request
      * @param Environment $twig
      * @param Conference $conference
      * @param CommentRepository $commentRepository
@@ -53,42 +61,31 @@ class ConferenceController extends AbstractController
      * @throws SyntaxError
      */
     public function show(
-        Environment $twig,
+        Request $request,
         Conference $conference,
         CommentRepository $commentRepository
 
     ) {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator(
+            $conference,
+            $offset
+        );
+
+
         return new Response(
-            $twig->render(
+            $this->twig->render(
                 'conference/show.html.twig',
                 [
                     'conference' => $conference,
-                    'comments' => $commentRepository->findBy(
-                        [
-                            'conference' =>
-                                $conference,
-                        ],
-                        ['createdAt' => 'DESC']
+                    'comments' => $paginator,
+                    'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                    'next' => min(
+                        count($paginator),
+                        $offset,
+                        CommentRepository::PAGINATOR_PER_PAGE
                     ),
                 ]
-            )
-        );
-    }
-
-    /**
-     * @Route("/test", name="test")
-     * @param Environment $twig
-     * @return Response
-     * @throws LoaderError
-     * @throws RuntimeError
-     * @throws SyntaxError
-     */
-    public function test(
-        Environment $twig
-    ) {
-        return new Response(
-            $twig->render(
-                'conference/test.html.twig'
             )
         );
     }
